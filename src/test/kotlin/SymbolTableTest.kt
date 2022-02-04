0586b65c9d015b6ac32caee8530993f51656dd9b
Mike Sorokin <mike-sorokin@outlook.com>
2022-02-04 17:07:29 +0000

    [test/ms2320] SymbolTableTest.kt: Added scoping tests

--- src/test/kotlin/SymbolTableTest.kt
+++ src/test/kotlin/SymbolTableTest.kt
@@ -1,6 +1,5 @@
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertIs
 import kotlin.test.assertTrue
 import kotlin.test.fail
 
@@ -101,6 +100,137 @@ class SymbolTableTest {
         val symbolTable = SymbolTable()
         try {
             symbolTable.reassign("x", 1337)
+            fail() // Should not get here
+        } catch (e: SemanticException) {
+            println(e)
+        } catch (e: Exception) {
+            fail()
+        }
+    }
+
+    @Test
+    fun canCreateChildScope() {
+        val st = SymbolTable()
+        val child = st.createChildScope()
+    }
+
+    @Test
+    fun declaringVariableInChildScopeDoesNotCreateItInParent() {
+        val st = SymbolTable()
+        val child = st.createChildScope()
+        child.declare("x", 5)
+        try {
+            st.get<Int>("x")
+            fail() // Should not get here
+        } catch (e: SemanticException) {
+            println(e)
+        } catch (e: Exception) {
+            fail()
+        }
+    }
+
+    @Test
+    fun childCanGetVariablesFromParent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        assertEquals(child.get("x"), 12)
+    }
+
+    @Test
+    fun childCanReassignDeclaredVariablesInParent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        st.reassign("x", 15)
+        assertEquals(st.get("x"), 15)
+        assertEquals(child.get("x"), 15)
+    }
+
+    @Test
+    fun childCanRedefineSameTypeVariable() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        child.declare("x", 20)
+        assertEquals(st.get("x"), 12)
+        assertEquals(child.get("x"), 20)
+    }
+
+    @Test
+    fun childCanDeclareSameNameButDifferentType() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        child.declare("x", "Hello!")
+        assertEquals(st.get("x"), 12)
+        assertEquals(child.get("x"), "Hello!")
+    }
+
+    @Test
+    fun childCannotReassignParentVariablesAfterNewDeclaration() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        st.declare("y", 20)
+        val child = st.createChildScope()
+        child.declare("x", 13)
+        child.declare("y", "Hi")
+        child.reassign("x", 16)
+        child.reassign("y", "Yo")
+
+        assertEquals(st.get("x"), 12)
+        assertEquals(st.get("y"), 20)
+    }
+
+    @Test
+    fun grandchildCanReassignGrandparent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        val grand = child.createChildScope()
+        grand.reassign("x", 15)
+        assertEquals(child.get("x"), 15)
+        assertEquals(st.get("x"), 15)
+    }
+
+    @Test
+    fun grandchildReassignsParentBeforeGrandparent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        child.declare("x", 15)
+        val grand = child.createChildScope()
+        grand.reassign("x", 17)
+        assertEquals(child.get("x"), 17)
+        assertEquals(st.get("x"), 12)
+    }
+
+    @Test
+    fun grandchildCannotReassignGrandparentIfRedeclareInParent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        child.declare("x", "Hi")
+        val grand = child.createChildScope()
+        try {
+            grand.reassign("x", 15)
+            fail() // Should not get here
+        } catch (e: SemanticException) {
+            println(e)
+        } catch (e: Exception) {
+            fail()
+        }
+    }
+
+    @Test
+    fun grandchildCannotGetGrandparentIfRedeclareInParent() {
+        val st = SymbolTable()
+        st.declare("x", 12)
+        val child = st.createChildScope()
+        child.declare("x", "Hi")
+        val grand = child.createChildScope()
+        try {
+            grand.get<Int>("x")
             fail() // Should not get here
         } catch (e: SemanticException) {
             println(e)
