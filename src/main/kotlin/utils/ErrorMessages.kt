0c6f36317f67581f09bdf06265b554a0802cfc1f
Andrey Popov <ap4220@ic.ac.uk>
2022-02-08 16:59:38 +0000

    [chore/ap4220] ErrorMessages.kt: Applied safe-delete to several unused functions in ErrorMessages.kt

--- src/main/kotlin/utils/ErrorMessages.kt
+++ src/main/kotlin/utils/ErrorMessages.kt
@@ -132,28 +132,6 @@ class SyntaxErrorMessageBuilder : ErrorMessageBuilder() {
     override val prefix = "SYNTAX ERROR"
 }
 
-fun main() {
-}
-
-fun raiseTypeErrorAndExit(ctx: ParserRuleContext?, expectedType: WAny?, actualType: WAny?) {
-    ctx?.run {
-        println("Line ${ctx.start.line}: Invalid operand expression type\n" +
-                "Expected: $expectedType, got: $actualType\"")
-        exitProcess(ExitCode.SEMANTIC_ERROR)
-    }
-    raiseNullContextError()
-}
-
-fun raiseNullContextError() {
-    println("Null Context Error")
-    exitProcess(ExitCode.UNKNOWN_ERROR)
-}
-
-fun raiseSemanticErrorAndExit() {
-    println("Semantic error!")
-    exitProcess(ExitCode.SEMANTIC_ERROR)
-}
-
 class SemanticException(private val reason: String) : Exception() {
     override val message: String
         get() = "Semantic error!\n$reason"
