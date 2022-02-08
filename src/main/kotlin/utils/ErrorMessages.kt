320aeda6ef95438348aac61864bd77abb6c0d5b1
Andrey Popov <ap4220@ic.ac.uk>
2022-02-08 22:26:40 +0000

    [chore/ap4220] ErrorMessages.kt: Added 'return this' to all builder methods

--- src/main/kotlin/utils/ErrorMessages.kt
+++ src/main/kotlin/utils/ErrorMessages.kt
@@ -1,9 +1,6 @@
 package utils
 
-import ast.ReturnStat
 import waccType.WAny
-import org.antlr.v4.runtime.ParserRuleContext
-import kotlin.system.exitProcess
 
 class PositionedError(
     private val lineNumber: Int,
@@ -51,16 +48,19 @@ abstract class ErrorMessageBuilder {
         return ErrorMessage(prefix, start, body)
     }
 
-    fun buildAndDisplay() {
+    fun buildAndDisplay(): ErrorMessageBuilder {
         println(build())
+        return this
     }
 
-    fun provideStart(lineNumber: Int, columnNumber: Int, lineText: String) {
+    fun provideStart(lineNumber: Int, columnNumber: Int, lineText: String): ErrorMessageBuilder {
         this.start = PositionedError(lineNumber, columnNumber, lineText)
+        return this
     }
 
-    fun provideStart(start: PositionedError) {
+    fun provideStart(start: PositionedError): ErrorMessageBuilder {
         this.start = start
+        return this
     }
 }
 
@@ -68,89 +68,110 @@ class SemanticErrorMessageBuilder : ErrorMessageBuilder() {
 
     override val prefix = "SEMANTIC ERROR"
 
-    fun readTypeIsIncorrect(actualType: WAny) {
+    fun readTypeIsIncorrect(actualType: WAny): SemanticErrorMessageBuilder {
         body += "Cannot read into the variable of type $actualType. Variables must be characters or numeric."
+        return this
     }
 
-    fun whileStatConditionHasNotBooleanType(actualType: WAny) {
+    fun whileStatConditionHasNotBooleanType(actualType: WAny): SemanticErrorMessageBuilder {
         whileStatConditionHasNotBooleanType()
         body += " Got $actualType instead"
+        return this
     }
 
-    fun whileStatConditionHasNotBooleanType() {
+    fun whileStatConditionHasNotBooleanType(): SemanticErrorMessageBuilder {
         body += "The conditional statement in the \"while\" statement does not evaluate to the boolean type."
+        return this
     }
 
-    fun ifStatConditionHasNotBooleanType(actualType: WAny) {
+    fun ifStatConditionHasNotBooleanType(actualType: WAny): SemanticErrorMessageBuilder {
         ifStatConditionHasNotBooleanType()
         body += " Got $actualType instead"
+        return this
     }
 
-    fun ifStatConditionHasNotBooleanType() {
+    fun ifStatConditionHasNotBooleanType(): SemanticErrorMessageBuilder {
         body += "The conditional statement in the \"if\" statement does not evaluate to the boolean type."
+        return this
     }
 
-    fun variableNotInScope(identifier: String) {
+    fun variableNotInScope(identifier: String): SemanticErrorMessageBuilder {
         body += "The variable $identifier is undefined in the scope it is accessed or any parent scope"
+        return this
     }
 
-    fun functionArgumentTypeMismatch(expectedType: WAny, actualType: WAny) {
+    fun functionArgumentTypeMismatch(expectedType: WAny, actualType: WAny): SemanticErrorMessageBuilder {
         body += "The supplied function argument has incorrect type. Expected $expectedType, got $actualType instead."
+        return this
     }
 
-    fun functionRedefineError(functionName: String) {
+    fun functionRedefineError(functionName: String): SemanticErrorMessageBuilder {
         body += "Cannot redefine function $functionName"
+        return this
     }
 
-    fun functionRedefineError() {
+    fun functionRedefineError(): SemanticErrorMessageBuilder {
         body += "Cannot redefine function"
+        return this
     }
 
-    fun functionArgumentCountMismatch(expectedArgumentsCount: Int) {
+    fun functionArgumentCountMismatch(expectedArgumentsCount: Int): SemanticErrorMessageBuilder {
         functionArgumentCountMismatch()
         body += "\nThere shall be $expectedArgumentsCount arguments"
+        return this
     }
 
-    fun functionArgumentCountMismatch() {
+    fun functionArgumentCountMismatch(): SemanticErrorMessageBuilder {
         body += "The number of provided arguments is incorrect"
+        return this
     }
 
-    fun functionReturnStatTypeMismatch(functionType: WAny, returnStatType: WAny) {
+    fun functionReturnStatTypeMismatch(functionType: WAny, returnStatType: WAny): SemanticErrorMessageBuilder {
         body += "The \"return\" statement of the function returns $returnStatType, " +
                 "but the function has type $functionType"
+        return this
     }
 
-    fun functionCallTypeMismatch(assignLhsType: WAny, functionType: WAny) {
+    fun functionCallTypeMismatch(assignLhsType: WAny, functionType: WAny): SemanticErrorMessageBuilder {
         body += "Cannot assign function of type $functionType to the variable of type $assignLhsType"
+        return this
     }
 
-    fun assignmentTypeMismatch(assignLhsType: WAny, assignRhsType: WAny) {
+    fun assignmentTypeMismatch(assignLhsType: WAny, assignRhsType: WAny): SemanticErrorMessageBuilder {
         body += "Cannot assign expression type $assignRhsType to the variable of type $assignLhsType"
+        return this
     }
 
-    fun returnFromGlobalScope() {
+    fun returnFromGlobalScope(): SemanticErrorMessageBuilder {
         body += "Cannot return from global scope!"
+        return this
     }
 
-    fun arrayEntriesTypeClash() {
+    fun arrayEntriesTypeClash(): SemanticErrorMessageBuilder {
         body += "The elements of the array have inconsistent types!"
+        return this
     }
 
-    fun arrayEntriesTypeMismatch(requiredType: WAny, actualType: WAny) {
+    fun arrayEntriesTypeMismatch(requiredType: WAny, actualType: WAny): SemanticErrorMessageBuilder {
         body += "The elements of the array of type $requiredType[] have incorrect type $actualType"
+        return this
     }
 
-    fun nonIntExpressionExit(actualType: WAny) {
+    fun nonIntExpressionExit(actualType: WAny): SemanticErrorMessageBuilder {
         body += "Cannot exit with non-int expression. Actual: $actualType"
+        return this
     }
 
-    fun nonArrayTypeElemAccess(nonArrayType: WAny) {
+    fun nonArrayTypeElemAccess(nonArrayType: WAny): SemanticErrorMessageBuilder {
         body += "Cannot access index elements of non-array type: $nonArrayType"
+        return this
     }
 
-    fun operandTypeMismatch(expectedType: WAny, actualType: WAny, expressionText: String = "") {
+    fun operandTypeMismatch(expectedType: WAny, actualType: WAny,
+                            expressionText: String = ""): SemanticErrorMessageBuilder {
         val expression = if (expressionText.isNotEmpty()) "\"$expressionText\"" else ""
         body += "The $actualType expression $expression does not conform to the expected type $expectedType"
+        return this
     }
 }
 
