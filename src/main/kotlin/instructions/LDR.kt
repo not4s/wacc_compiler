00c0c789431c412b0ef7283a4a64723c69ce7b8a
Jaeho Chung <jc2520@ic.ac.uk>
2022-02-21 06:09:46 +0000

    [feat/jc2520] LDR.kt: implementation of the ARM LDR/LDRSB instruction

---
+++ src/main/kotlin/instructions/LDR.kt
@@ -0,0 +1,13 @@
+package instructions
+
+class LDR(val rDest:        Register,
+          val op2:          Operand2,
+          var isSignedByte: Boolean = false) {
+    
+    override fun toString(): String {
+        var sb = "LDR"
+        if(isSignedByte) sb.plus("SB")
+        sb.plus(" " + rDest + ", " + op2)
+        return sb
+    }
+}
