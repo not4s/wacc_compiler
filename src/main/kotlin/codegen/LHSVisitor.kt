843b71ece27463fa79ae6f26d7f9578bcf19f491
Mike Sorokin <ms2320@ic.ac.uk>
2022-03-04 13:01:54 +0000

    [fix/ms2320] Pair assignmnet fix

--- src/main/kotlin/codegen/LHSVisitor.kt
+++ src/main/kotlin/codegen/LHSVisitor.kt
@@ -4,34 +4,40 @@ import ast.ArrayElement
 import ast.IdentifierSet
 import ast.LHS
 import ast.PairElement
-import instructions.operations.*
 import instructions.WInstruction
 import instructions.misc.DataDeclaration
 import instructions.misc.Operand2
+import instructions.misc.Register
+import instructions.operations.MOV
+import instructions.operations.POP
+import instructions.operations.PUSH
+import instructions.operations.STR
 
 class LHSVisitor(
     private val data: DataDeclaration,
     private val registerProvider: RegisterProvider,
     private val funcPool: FunctionPool,
-    ) : ASTVisitor<LHS> {
+) : ASTVisitor<LHS> {
 
     var resultStored: Operand2? = null
 
     override fun visit(ctx: LHS): List<WInstruction> {
         return when (ctx) {
             is IdentifierSet -> {
-                val freeRegister = registerProvider.get()
-                resultStored = freeRegister
-                registerProvider.ret()
-                ctx.st.asmGet(ctx.identifier, freeRegister, data)
+                ctx.st.asmAssign(ctx.identifier, Register.resultRegister(), data, null)
             }
             is ArrayElement -> TODO()
 
             is PairElement -> {
-                val exprReg = registerProvider.get()
-                val pairElemReg = registerProvider.get()
-                return ExprVisitor(data, registerProvider, funcPool).visit(ctx.expr).
-                        plus(STR(exprReg, pairElemReg, 0))
+                return listOf<WInstruction>(
+                    PUSH(Register.resultRegister(), data),
+                ).plus(ExprVisitor(data, registerProvider, funcPool).visit(ctx.expr))
+                    .plus(POP(Register("r1"), data)).plus(
+                        STR(Register.resultRegister(), Register.resultRegister(), offset = if (ctx.first) 0 else 4)
+
+                    ).plus(
+                        MOV(Register.resultRegister(), Register("r1"))
+                    )
             }
             else -> throw Exception("Unknown LHS $ctx")
         }
